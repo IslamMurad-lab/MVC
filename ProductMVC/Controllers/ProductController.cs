4f6a840ed969379c113dc507a9b9e8bd502d467c
Islam Murad <___>
2026-09-03 15:31:58 +0300

    Add project files.

---
+++ ProductMVC/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductMVC.Models;
+
+namespace ProductMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductRepository productRepository = new ProductRepository();
+
+        public IActionResult GetAll()
+        {
+            var products = productRepository.GetAllProducts();
+
+            return View(products);
+        }
+
+        public IActionResult GetById(int id)
+        {
+            var product = productRepository.GetProductById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            productRepository.AddNewProduct(product);
+
+            return RedirectToAction("GetAll");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var product = productRepository.GetProductById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Product product)
+        {
+            productRepository.UpdateProduct(product);
+
+            return RedirectToAction("GetAll");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            productRepository.DeleteProduct(id);
+
+            return RedirectToAction("GetAll");
+        }
+    }
+}
---
+++ ProductMVC/Models/ProductRepository.cs
@@ -0,0 +1,72 @@
+﻿using ProductMVC.Models;
+
+namespace ProductMVC.Models
+{
+    public class ProductRepository
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product
+            {
+                Id = 1,
+                Name = "Smart Watch",
+                Price = 2500,
+                ImageUrl = "/assets/smartwatch.jfif"
+            },
+
+            new Product
+            {
+                Id = 2,
+                Name = "Phone",
+                Price = 15000,
+                ImageUrl = "/assets/phone.jfif"
+            },
+
+            new Product
+            {
+                Id = 3,
+                Name = "PC",
+                Price = 30000,
+                ImageUrl = "/assets/PC.jfif"
+            }
+        };
+
+        public List<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public Product GetProductById(int id)
+        {
+            return products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void AddNewProduct(Product product)
+        {
+            product.Id = products.Max(p => p.Id) + 1;
+            products.Add(product);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            var existingProduct = products.FirstOrDefault(p => p.Id == product.Id);
+
+            if (existingProduct != null)
+            {
+                existingProduct.Name = product.Name;
+                existingProduct.Price = product.Price;
+                existingProduct.ImageUrl = product.ImageUrl;
+            }
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+
+            if (product != null)
+            {
+                products.Remove(product);
+            }
+        }
+    }
+}
